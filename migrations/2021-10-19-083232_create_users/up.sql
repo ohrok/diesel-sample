396b53e664c70a4bd64d6e072500a02dcb230048
ohrok <oh.hymh@gmail.com>
2021-10-24 16:32:26 +0900

    fix show_posts

--- migrations/2021-10-19-083232_create_users/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE users (
-  id UUID PRIMARY KEY,
-  name TEXT NOT NULL,
-  username TEXT NOT NULL UNIQUE
-)
--- src/bin/show_posts.rs
+++ src/bin/show_posts.rs
@@ -1,24 +1,26 @@
-extern crate diesel;
-extern crate diesel_sample;
-
-use self::diesel::prelude::*;
-use self::diesel_sample::*;
-use self::models::*;
+use diesel::prelude::*;
+use diesel_sample::*;
+use models::post::Post;
+use models::user::User;
 
 fn main() {
     use diesel_sample::schema::posts::dsl::*;
+    use diesel_sample::schema::users::dsl::*;
 
     let connection = establish_connection();
     let results = posts
-        .filter(published.eq(true))
         .limit(5)
         .load::<Post>(&connection)
         .expect("Error loading posts");
 
     println!("Displaying {} posts", results.len());
     for post in results {
-        println!("{}", post.title);
-        println!("----------\n");
+        println!("\n{}", post.title);
+        println!("----------");
         println!("{}", post.body);
+        println!("----------");
+
+        let user: User = users.find(post.user_id).get_result(&connection).unwrap();
+        println!("by {}", user.username);
     }
 }
