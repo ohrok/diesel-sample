cc710c0b357bd1a3ebbfc48cc929ab720a9fb4c8
ohrok <oh.hymh@gmail.com>
2021-10-24 17:28:40 +0900

    add update_post

---
+++ src/bin/update_post.rs
@@ -0,0 +1,54 @@
+use diesel::prelude::*;
+use diesel_sample::*;
+use models::post::Post;
+use std::io::{stdin, Read};
+use uuid::Uuid;
+
+fn main() {
+    use diesel_sample::schema::posts::dsl::{body, posts, title};
+
+    println!("Input the post id!");
+    let id = {
+        let mut s = String::new();
+        stdin().read_line(&mut s).unwrap();
+        let s = s.trim();
+        match Uuid::parse_str(s) {
+            Ok(id) => id,
+            Err(error) => {
+                println!("Error parsing the entered post id to UUID: {}", error);
+                return;
+            }
+        }
+    };
+
+    println!("\nInput new title!");
+    let new_title = {
+        let mut s = String::new();
+        stdin().read_line(&mut s).unwrap();
+        s.trim().to_owned()
+    };
+
+    println!(
+        "\nOk! Let's write {} (Press {} when finished)\n",
+        new_title, EOF
+    );
+    let new_body = {
+        let mut s = String::new();
+        stdin().read_to_string(&mut s).unwrap();
+        s.trim().to_owned()
+    };
+
+    let connection = establish_connection();
+
+    let post = diesel::update(posts.find(id))
+        .set((title.eq(new_title), body.eq(new_body)))
+        .get_result::<Post>(&connection)
+        .expect(&format!("Unable to find post {}", id));
+    println!("\nUpdated post {}", post.title);
+}
+
+#[cfg(not(windows))]
+const EOF: &'static str = "CTRL+D";
+
+#[cfg(windows)]
+const EOF: &'static str = "CTRL+Z";
